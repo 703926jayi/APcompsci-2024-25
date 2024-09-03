566e0ca1e6befc0579a6739be68338d592e004d0
703926jayi <jayinaganti1@gmail.com>
2024-09-03 08:41:58 -0400

    stuff

---
+++ Print_vs_Println.java
@@ -0,0 +1,7 @@
+public class Print_vs_Println {
+    public static void main(String[] args) {
+        int age = 54;
+        System.out.println("I am");
+        System.out.print(age); //doesnt change lines
+    }
+}
---
+++ Variables.java
@@ -0,0 +1,10 @@
+public class Variables {
+    //int double boolean char
+    //String
+    public static void main(String[] args) {
+        String name = "big text line";
+        System.out.println(name);
+        //use final to prevent changes to variables
+        
+    }
+}
