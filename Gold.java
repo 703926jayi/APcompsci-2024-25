684a5417e645bbc1acd68f65ecdcadcad0ff15db
703926jayi <jayinaganti1@gmail.com>
2024-09-16 09:32:34 -0400

    ahh

---
+++ Gold.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class Gold {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        int card = s.nextInt();
+        if (card/100000  >= 1000000000){
+            System.out.println();
+        }
+    }
+}
