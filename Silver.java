d28f0b10a178ae13adce0d4fc170bed675befe64
703926jayi <jayinaganti1@gmail.com>
2025-01-13 09:33:10 -0500

    afdss

--- Silver.java
+++ Silver.java
@@ -7,13 +7,13 @@ public class Silver {
         
         for(int i = 0; i < bs.length();i++){
            int c = bs.charAt(i);
-           bs.replace("you","U");
+           bs.replaceAll("you","U");
 
-            bs.replace("y "," ");
-            bs.replace("and","&");
-            bs.replace("to","2");
-            
-            bs.replace("for","4");
+            bs.replaceAll("y "," ");
+            bs.replaceAll("and","&");
+            bs.replaceAll("to","2");
+
+            bs.replaceAll("for","4");
 
             if(c != 85 && c != 32 && c ==65 ||c == 69 || c == 73|| c == 79 || c ==97 || c == 101 || c ==105|| c == 111|| c == 117){
                 bs = bs.substring(0,c) + bs.substring(c, bs.length());
