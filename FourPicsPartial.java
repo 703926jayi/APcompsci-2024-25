d95b252e55da4c60c20095279e27f1d39558e0bd
703926jayi <jayinaganti1@gmail.com>
2025-02-27 09:43:26 -0500

    asfagsgdshr

--- FourPicsPartial.java
+++ FourPicsPartial.java
@@ -1,4 +1,3 @@
-
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
@@ -34,7 +33,7 @@ public static ArrayList<String> buildDictionary()
 	}
 	while (input.hasNext())
 	{
-		String word = input.nextLine().toLowerCase();  //one word appears on each line in the text file
+		String word = input.nextLine();  //one word appears on each line in the text file
 		wordList.add(word); 			//add each word into the wordList ArrayList
 	}
 	input.close();
@@ -45,48 +44,65 @@ public static void getSizeAndLetters()
 {
 	Scanner s=  new Scanner(System.in);
 	System.out.println("Enter number of letters in word:");
+	numLetters = s.nextInt();  //numLetters specifies the number of letters in the word we are searching for 
 	System.out.println("Enter available letters as a block of 12 letters:  (ex:  psbnoiyzvlvw)");
 	String block = s.next();
 	letters=block.toCharArray();  //build an array of characters from the word provided by user
 }
 public static void checkLength()
 	{
-		for (int i = 0; i < dictionary.size(); i++)  
+		for (int i=0; i<dictionary.size(); i++)  
 		{
 			if (dictionary.get(i).length() != numLetters) 
 			{
 				dictionary.remove(i);  
-				i--;  
+				i--; 
 			}
 		}
 		System.out.println("After eliminated words of different length, " + dictionary.size() + " remain.");
 	}
 public static void  checkLetters()  //checks to make sure all letters in the word are included in the block of 12 letters
 	{
-		for (int i = 0; i < dictionary.size(); i++)  
+		for (int i=0; i<dictionary.size(); i++)  
 		{
-			String word = dictionary.get(i);
-			for (int j = 0; j < word.length(); j++) 
+			if (!containsChar(dictionary.get(i), letters)) 
 			{
-				if (!containsChar(word, letters)) 
-				{
-					dictionary.remove(i);  
-					i--;  
-					break;
-				}
+				dictionary.remove(i);  
+				i--; 
 			}
 		}
 		System.out.println("After checking the letters provided to those left,  " + dictionary.size() + " remain.");
-	}
+	} 
 public static boolean containsChar(String st, char[] c)
 	{
+		for (int i=0; i<st.length(); i++)  
+		{
+			boolean found = false;
+			for (int j=0; j<c.length; j++)  
+			{
+				if (st.charAt(i) == c[j]) 
+				{  
+					//make sure does not return true if duplicate letters are there
+					// if (st.indexOf(st.charAt(i)) != st.lastIndexOf(st.charAt(i))) 
+					// {
+					// 	return false;
+					// }
+					found = true;
+					break;
+				}
+			}
+			if (!found) 
+			{
+				return false;
+			}
+		}
 		return true;
 	}
 public static void outputWords()
 	{
-		for (int i = 0; i < dictionary.size(); i++) 
+		for (int i=0; i<dictionary.size(); i++)  
 		{
-			System.out.println(dictionary.get(i));
+			System.out.println(dictionary.get(i));  
 		}
 		System.out.println("Total number of words found: " + dictionary.size());
 	}
