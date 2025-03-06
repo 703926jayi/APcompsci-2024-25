ef8f31f6bdb8c55fbe784be94daa849543d74266
703926jayi <jayinaganti1@gmail.com>
2025-03-06 09:12:36 -0500

    recursion

--- Recursion2.java
+++ Recursion2.java
@@ -4,15 +4,13 @@ public class Recursion2
     {
         //Problem # 1 - Recursive Palindrome Checker
         String st = "racecar";
-        if(isPalindrome(st)) System.out.println(st + " is a palindrom");
+        if(isPalindrome(st)) System.out.println(st + " is a palindrome");
        else System.out.println(st+" is not a palindrome");
-
-       
        
         //Problem # 2 - Sum Digits in an Integer
         int num = 3874;
         System.out.println(sumDigits(num));
-       
+        
         //Problem # 3 - Reverse String
         String st2 = "dixon";
         System.out.println(reverseString(st2));
@@ -23,21 +21,26 @@ public class Recursion2
     }
     public static boolean isPalindrome(String st)
     {
-        //recursively solve this
-        return false;
+//this method checks to see if is a palindrome using recursion 
+        if(st.length()<=1) return true; //basecase 
+        if(st.charAt(0)!=st.charAt(st.length()-1)) return false; 
+            else return isPalindrome(st.substring(1,st.length()-1));
     }
     public static int sumDigits(int n)
     {
-        return 0;
-       
+        if(n<10) return n; //basecase
+        return n%10 + sumDigits(n/10);
     }
     public static String reverseString(String st)
     {
-        return "";
+        if(st.length()<=1) return st; //basecase
+        return st.charAt(st.length()-1) + reverseString(st.substring(0,st.length()-1));
     }
    
     public static String removeRepeats(String st)
     {
-        return "";
+        if(st.length()<=1) return st; //basecase
+        if(st.charAt(0)==st.charAt(1)) return removeRepeats(st.substring(1));
+        return st.charAt(0) + removeRepeats(st.substring(1));
     }
 }
